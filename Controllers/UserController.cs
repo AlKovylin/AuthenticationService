f2547416c924d99284a625a8682428ef0d1a63e7
Ковылин Алексей <alkovylin@ya.ru>
2022-05-22 22:26:33 +0300

    Добавьте файлы проекта.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace AuthenticationService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        public UserController(ILogger logger, IMapper mapper)
+        {
+            _logger = logger;
+            _mapper = mapper;
+
+            _logger.WriteEvent("Сообщение о событии в программе");
+            _logger.WriteError("Сообщение об ошибке в программе");
+        }
+
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "ivan@gmail.com",
+                Password = "111222",
+                Login = "ivanov"
+            };
+        }
+
+        [HttpGet]
+        [Route("viewmodel")]
+        public UserViewModel GetUserViewModel()
+        {
+            User user = new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Иван",
+                LastName = "Иванов",
+                Email = "ivan@gmail.com",
+                Password = "11111122222qq",
+                Login = "ivanov"
+            };
+
+            var userViewModel = _mapper.Map<UserViewModel>(user);
+            
+            return userViewModel;
+        }
+    }
+}
---
+++ MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace AuthenticationService
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserViewModel>().ConstructUsing(v => new UserViewModel(v));
+        }
+    }
+}
